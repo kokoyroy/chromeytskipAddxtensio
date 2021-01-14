e544d26cd325b52b9cfa72c68aebd40e8e8c05d6
kokoyroy <44644032+kokoyroy@users.noreply.github.com>
2021-01-14 04:04:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ const timer = setInterval(() => {
     const skipbtn = document.querySelector('.ytp-ad-skip-button.ytp-button')
     if (skipbtn != undefined) {
         skipbtn.click()
-        // clearInterval(timer);
     }
 }, 1000)
