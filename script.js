bb648a0766ffdd65188bab834771300fdcc86006
kostas koyroyklidis <kostaskoyroyklidis@kostass-MacBook-Pro.local>
2023-12-03 21:01:47 +0200

    edited class name

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 setInterval(() => {
-    const skipbtn = document.querySelector('.ytp-ad-skip-button.ytp-button')
-    const smalladd = document.querySelector('.ytp-ad-overlay-close-button')
-    
-    
-    if (skipbtn != undefined) {
-        console.log('ekleisa ti diafimisi ti megali');
-        skipbtn.click()
-    }
-    if (smalladd != undefined) {
-        console.log('ekleisa ti diafimisi ti mikri');
-        smalladd.click()
-    }
-}, 1000)
+  const skipbtn = document.querySelector(".ytp-ad-skip-button-slot");
+  const smalladd = document.querySelector(".ytp-ad-overlay-close-button");
+
+  if (skipbtn != undefined) {
+    console.log("ekleisa ti diafimisi ti megali");
+    skipbtn.click();
+  }
+  if (smalladd != undefined) {
+    console.log("ekleisa ti diafimisi ti mikri");
+    smalladd.click();
+  }
+}, 1000);
